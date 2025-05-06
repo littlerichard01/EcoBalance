dedcb654d35072883c93ab8227aad22aca3af911
Ricardo Campos <106117682+littlerichard01@users.noreply.github.com>
2025-05-06 17:51:30 -0300

    Alteração para incluir http//

--- client/src/pages/InfoCadastro.js
+++ client/src/pages/InfoCadastro.js
@@ -120,7 +120,7 @@ const InfoCadastro = () => {
 
         try {
             const response = await axios.put(
-                `ecobalance-backend.onrender.com/api/usuarios/${usuarioAtual._id}`,
+                `https://ecobalance-backend.onrender.com/api/usuarios/${usuarioAtual._id}`,
                 usuarioAtualizado
             );
 
